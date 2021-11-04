a6ddad78f4d02b361f3a8321d7abe317b958693d
Felipe Cardoso <felipedclc@gmail.com>
2021-11-04 09:41:06 -0300

    adiciona variavel de ambiente para url api back end

--- src/api/backEndApi.js
+++ src/api/backEndApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const backEndApi = axios.create({
-  baseURL: 'http://localhost:3001/',
+  baseURL: process.env.REACT_API_BACK_URL || 'http://localhost:3001/',
 });
 
 export default backEndApi;
